56837bd33622f419486fa6804929053296981181
onidarf112 <fradinorscia998@gmail.com>
2025-06-19 12:05:34 +0200

    Update tool_scambi_fantacalcio_avanzato.py

--- tool_scambi_fantacalcio_avanzato.py
+++ tool_scambi_fantacalcio_avanzato.py
@@ -5,8 +5,8 @@ from scipy.stats import percentileofscore
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.set_page_config(page_title="Fantacalcio - Scambi Avanzati Pro", layout="wide")
-st.title("⚽ Fantacalcio - Tool Scambi Avanzati Pro")
+st.set_page_config(page_title="Fantacalcio - Scambi Avanzati ", layout="wide")
+st.title("⚽ Fantacalcio - Tool Scambi Avanzati by Onidarf")
 
 # Sidebar per configurazioni
 st.sidebar.header("⚙️ Configurazioni")
